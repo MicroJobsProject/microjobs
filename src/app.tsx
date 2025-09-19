65c9d1b39d0a6b94151c13d1e600ffb3b2a1e989
pablo-sch <pablsch.it@gmail.com>
2025-09-19 20:27:26 +0200

    added RequireNoAuth component to protect routes from authenticated users.

--- src/app.tsx
+++ src/app.tsx
@@ -7,9 +7,9 @@ import RegisterPage from "./pages/auth/RegisterPage";
 
 const LoginPage = lazy(() => import("./pages/auth/LoginPage"));
 //const RequireAuth = lazy(() => import("./components/auth/RequireAuth"));
+const RequireNoAuth = lazy(() => import("./components/auth/RequireNoAuth"));
 
 const Home = lazy(() => import("./pages/Home"));
-
 const Layout = lazy(() => import("./components/layout/layout"));
 
 function App() {
@@ -17,8 +17,22 @@ function App() {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
+          <Route
+            path="login"
+            element={
+              <RequireNoAuth>
+                <LoginPage />
+              </RequireNoAuth>
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <RequireNoAuth>
+                <RegisterPage />
+              </RequireNoAuth>
+            }
+          />
 
           <Route path="home" element={<Home />} />
 
