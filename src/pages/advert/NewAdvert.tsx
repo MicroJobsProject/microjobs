63998365958c1cc4825ad683e44d604e8537f633
Pablo Cervigon <pablocervigon@gmail.com>
2025-09-29 23:57:33 +0200

    radio button style updated in index.css

--- src/pages/advert/NewAdvert.tsx
+++ src/pages/advert/NewAdvert.tsx
@@ -25,113 +25,104 @@ function NewAdvertPage() {
   return (
     <>
       <div className="wrapper">
-        <div className="flex min-h-screen items-center justify-center">
-          <div className="w-full max-w-md">
-            <h2 className="text-heading font-heading mb-2 text-3xl font-extrabold">
-              Create new Advert
-            </h2>
-            <p className="text-paragraph">
-              Post your service need or offer to connect with local
-              professionals and particulars.
-            </p>
-            <div className="bg-container border-border rounded-xl border p-6 shadow-sm">
-              <form onSubmit={handleSubmit} className="space-y-6">
-                <div className="flex flex-col lg:col-span-2">
-                  <label htmlFor="name" className="mb-2 font-medium">
-                    Title*
-                  </label>
-                  <div>
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="eg., Need experienced plumber for kitchen renovation"
-                      required
-                      className="h-10.5 w-full rounded-lg border border-gray-200 px-3 py-2 placeholder:text-gray-200"
-                    />
-                  </div>
-                </div>
-
-                <div className="flex flex-col lg:col-span-2">
-                  <label htmlFor="price" className="mb-2 font-medium">
-                    Price*
-                  </label>
-                  <input
-                    type="number"
-                    name="price"
-                    placeholder="0.00€"
-                    required
-                    className="h-10.5 w-full rounded-lg border border-gray-200 px-3 py-2 placeholder:text-gray-200"
-                  />
-                </div>
-
-                <div>
-                  <fieldset
-                    name="serviceFieldset"
-                    className="flex flex-row items-center justify-center gap-4"
-                  >
-                    <div className="flex grow flex-row items-center justify-center">
-                      <input
-                        id="need"
-                        type="radio"
-                        name="serviceRadio"
-                        value="need"
-                        defaultChecked
-                        className="peer hidden"
-                      />
-                      <label
-                        htmlFor="need"
-                        className="peer-checked:bg-primary border-border grow cursor-pointer rounded-md border px-2 py-1 text-center peer-checked:text-white"
-                      >
-                        Need Service
-                      </label>
-                    </div>
-                    <div className="flex grow flex-row items-center justify-center">
-                      <input
-                        id="offer"
-                        type="radio"
-                        name="serviceRadio"
-                        value="offer"
-                        className="peer hidden"
-                      />
-                      <label
-                        htmlFor="offer"
-                        className="peer-checked:bg-primary border-border grow cursor-pointer rounded-md border px-2 py-1 text-center peer-checked:text-white"
-                      >
-                        Offer Service
-                      </label>
-                    </div>
-                  </fieldset>
-                </div>
-
-                <div className="flex flex-col lg:col-span-2">
-                  <label htmlFor="description" className="mb-2 font-medium">
-                    Description
-                  </label>
-
-                  <input
-                    type="text"
-                    name="description"
-                    placeholder="Description"
-                    required
-                    className="h-10.5 w-full rounded-lg border border-gray-200 px-3 py-2 placeholder:text-gray-200"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="category">Category</label>
-                  <input
-                    type="text"
-                    name="category"
-                    placeholder="Category"
-                    required
-                  />
-                </div>
-
-                <button type="submit" className="btn btn-primary">
-                  Create Advert
-                </button>
-              </form>
+        <h2 className="text-heading font-heading mb-2 text-3xl font-extrabold">
+          Create new Advert
+        </h2>
+        <p className="text-paragraph mb-2">
+          Post your service need or offer to connect with local professionals
+          and particulars.
+        </p>
+        <div className="bg-container border-border rounded-xl border p-6 shadow-sm">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="flex flex-col lg:col-span-2">
+              <label htmlFor="name" className="mb-2 font-medium">
+                Title*
+              </label>
+              <div>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="eg., Need experienced plumber for kitchen renovation"
+                  required
+                  className="input"
+                />
+              </div>
             </div>
-          </div>
+
+            <div className="flex flex-col lg:col-span-2">
+              <label htmlFor="price" className="mb-2 font-medium">
+                Price*
+              </label>
+              <input
+                type="number"
+                name="price"
+                placeholder="0.00€"
+                required
+                className="input"
+              />
+            </div>
+
+            <div>
+              <fieldset
+                name="serviceFieldset"
+                className="flex flex-row items-center justify-center gap-4"
+              >
+                <legend className="mb-2 font-medium">Advert Type</legend>
+                <div className="flex grow flex-row items-center justify-center">
+                  <input
+                    id="need"
+                    type="radio"
+                    name="serviceRadio"
+                    value="need"
+                    defaultChecked
+                    className="peer hidden"
+                  />
+                  <label htmlFor="need" className="input-radio grow">
+                    Need Service
+                  </label>
+                </div>
+                <div className="flex grow flex-row items-center justify-center">
+                  <input
+                    id="offer"
+                    type="radio"
+                    name="serviceRadio"
+                    value="offer"
+                    className="peer hidden"
+                  />
+                  <label htmlFor="offer" className="input-radio grow">
+                    Offer Service
+                  </label>
+                </div>
+              </fieldset>
+            </div>
+
+            <div className="flex flex-col lg:col-span-2">
+              <label htmlFor="description" className="mb-2 font-medium">
+                Description
+              </label>
+
+              <input
+                type="text"
+                name="description"
+                placeholder="Description"
+                required
+                className="h-10.5 w-full rounded-lg border border-gray-200 px-3 py-2 placeholder:text-gray-200"
+              />
+            </div>
+            <div>
+              <label htmlFor="category">Category</label>
+              <input
+                type="text"
+                name="category"
+                placeholder="Category"
+                required
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Create Advert
+            </button>
+          </form>
         </div>
       </div>
     </>
