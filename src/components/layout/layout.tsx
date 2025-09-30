7c3e1dc0a8633d3b7fa7bf2d670e5249802dbd7a
Miguel Fernandez <miguelferlez@gmail.com>
2025-09-30 20:33:34 +0200

    wip: update path after changing language

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,20 +1,31 @@
 //DEPENDENCIES
-import { Outlet } from "react-router";
+import { Outlet, useParams } from "react-router";
 
 //NATIVE
 import Footer from "./Footer";
 import Header from "./Header";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 function Layout() {
+  const { i18n } = useTranslation();
+  const { lang } = useParams();
+
+  useEffect(() => {
+    if (lang && i18n.language !== lang) {
+      i18n.changeLanguage(lang);
+    }
+  }, [lang, i18n]);
+
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <h1 className="sr-only">MicroJobs</h1>
       <Header />
-      <main>
+      <main className="flex flex-1 items-center justify-center">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/components/ui/LanguageSelector.tsx
+++ src/components/ui/LanguageSelector.tsx
@@ -1,16 +1,25 @@
 //DEPENDENCIES
 import { useTranslation } from "react-i18next";
+// import { useLocation, useNavigate, useParams } from "react-router";
 import clsx from "clsx";
 
 function LanguageSelector() {
   const { i18n } = useTranslation();
+  // const { lang } = useParams();
+  // const location = useLocation();
+  // const navigate = useNavigate();
   const languages = [
     { code: "en", lang: "English" },
     { code: "es", lang: "Español" },
   ];
 
   function handleChangeLanguage(newLanguage: string) {
+    //TODO: update URL path after language change
+    // const currentPath = location.pathname.replace(`/${lang}`, "");
+
     i18n.changeLanguage(newLanguage);
+
+    // navigate(`/${newLanguage}${currentPath}${location.search}`);
   }
 
   return (
