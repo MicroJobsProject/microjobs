dab4203629979e4a24a6e07673ffb4ce8e277539
Miguel Fernandez <miguelferlez@gmail.com>
2025-10-03 17:56:01 +0200

    refactor: move static locales to public

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -2,18 +2,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-
-//STATIC FILES
-import enJSON from "../assets/locales/en.json";
-import esJSON from "../assets/locales/es.json";
+import Backend from "i18next-http-backend";
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: { translation: enJSON },
-      es: { translation: esJSON },
+    backend: {
+      loadPath: "/locales/{{lng}}.json",
     },
     fallbackLng: "en",
     returnObjects: true,
